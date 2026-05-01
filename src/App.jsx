8c824aaefa3936bb8973294a6bd4b173d7c6675a
robert <robert.zibordi@aluno.cps.sp.gov.br>
2026-04-30 22:18:15 -0300

    inicia projeto copa!

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>world cup 2026</header>
+      <nav>
+        <a href="home">HOME</a>
+        <a href="#jogos">JOGOS</a>
+        <a href="#historia">A HISTORIA</a>
+      </nav>
+      <section id=""></section>
+      <section id="home"></section>
+      <section id="jogos"></section>
+      <section id="historia"></section>
+    </div>
+  );
+}
+
+export default App;
